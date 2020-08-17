92b37684f4b78320cf44b735e688d6db79bc3728
Yumi Patton <yumipatton@gmail.com>
2020-08-17 16:47:30 +1000

    test with jasmine and refactor

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-/*part1 Let Sunday =0 Monday = 1, Tuesday=2 Wednesday=3 Thursday=4 Friday =5 Saturday=6 */
-function businessHours(dayNumber, hourNumber) {
-    return hourNumber >= 9 && hourNumber <= 18 && dayNumber >= 1 && dayNumber <= 5;
+/*Let Sunday =0 Monday = 1, Tuesday=2 Wednesday=3 Thursday=4 Friday =5 Saturday=6
+business hours is between 9-18 */
+
+class BusinessHoursFn{
+    constructor(janFirstDayNumber){
+       this.janFirstDayNumber = janFirstDayNumber;
+       this.dayNumber = null;
+       this.date; 
+    }
+    //Part 1
+    businessHours(dayNumber, hourNumber) {
+        return hourNumber >= 9 && hourNumber <= 18 && dayNumber >= 1 && dayNumber <= 5;
+    };
+    //Part 2
+    getDayNumber(yearDayNumber) {
+        if (yearDayNumber < 0 || yearDayNumber > 365){
+            return undefined;
+        }
+        return (this.janFirstDayNumber + yearDayNumber) % 7;
+    };
+    //Part3
+    businessHoursDate(yearDayNumber, hourNumber) {
+        this.dayNumber = this.getDayNumber(this.janFirstDayNumber, yearDayNumber);
+        return this.businessHours(this.dayNumber, hourNumber);
+    };
+    //Challenge
+    //E.g. January 24 2019 at 10 => "2019-1-25-10:00",  Dec 21 Sat 2019 at 5 pm  => "2019-1-15-10:00"
+    businessHoursDateChallenge(dateString) {
+        this.date = new Date(dateString);
+        return this.businessHours(this.date.getDay(), this.date.getHours());
+    };    
 }
-/* Monday at 8 pm*/
-//console.log("Monday at 8pm is " + businessHours(1, 20));
-//Part 2
-function getDayNumber(janFirstDayNumber, yearDayNumber) {
-    return (janFirstDayNumber + yearDayNumber) % 7;
-}
-//Jan 24 Thursday 2019
-//console.log("January 24nd Thur 2019 is " + getDayNumber(2, 23));
-//Dec 31 Tuesday 2019
-//console.log("Dec 31 Tues 2019 is " + getDayNumber(2, 364));
-//Part3
-function businessHoursDate(janFirstDayNumber, yearDayNumber, hourNumber) {
-    const dayNumber = getDayNumber(janFirstDayNumber, yearDayNumber);
-    return businessHours(dayNumber, hourNumber);
-}
-//console.log("Dec 21 Sat 2019 at 5 pm is " + businessHoursDate(2, 354, 17));
-//console.log("January 24nd Thur 2019 at 10 am is " + businessHoursDate(2, 23, 10));
-//Challenge
-//E.g. January 24 2019 at 10 => "2019-1-25-10:00",  Dec 21 Sat 2019 at 5 pm  => "2019-1-15-10:00"
-function businessHoursDateChallenge(dateString) {
-    const date = new Date(dateString);
-    return businessHours(date.getDay(), date.getHours());
-}
-//console.log("Dec 21 Sat 2019 at 5 pm is " + businessHoursDateChallenge("2019-12-21-17:00"));
-//console.log("January 24nd Thur 2019 at 10 am is " + businessHoursDateChallenge("2019-1-25-10:00"));
+
+
+module.exports = BusinessHoursFn;
